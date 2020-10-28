91c3e807e6eff8e9dc932cac53843c5636d83fc1
Irina Turchina <turkina.ira@gmail.com>
2020-10-28 14:40:04 +0200

    resolved conflict btw branch1 and main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,15 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello world again");
     }
+    public static String firstMethod (String s) {
+        s = "Hello";
+        return s;
+    }
+
+    public static String secondMethod (String s) {
+        s = "World";
+        return s;
+    }
 
     public static int firstMethod (int c) {
         return c;
