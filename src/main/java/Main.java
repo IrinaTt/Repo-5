823a1024f230bcb0cb6790ddd425c009194c9472
Irina Turchina <turkina.ira@gmail.com>
2020-10-28 17:50:13 +0200

    added method three

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,24 +3,30 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello world again");
     }
-    public static String firstMethod (String s) {
+
+    public static String firstMethod(String s) {
         s = "Hello";
         return s;
     }
 
-    public static String secondMethod (String s) {
+    public static String secondMethod(String s) {
         s = "World";
         return s;
     }
 
-    public static int firstMethod (int c) {
+    public static int firstMethod(int c) {
         return c;
     }
 
-    public static int secondMethod (int c) {
+    public static int secondMethod(int c) {
         return c;
     }
-    public static int two (int c) {
+
+    public static int two(int c) {
+        return c;
+    }
+
+    public static int three(int c) {
         return c;
     }
 }
